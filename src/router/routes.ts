bb57056dbcbd1cfaf9868a18252c928682432607
aliyun2145454989 <2283582431@qq.com>
2023-05-25 16:42:11 +0800

    feat(custom): 业主开屏动画

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,87 +1,107 @@
-import Layout from "@/layout/index.vue";
-import type { RouteRecordRaw } from "vue-router";
-import Home from "@/views/home/index.vue";
-import Login from "@/views/login/index.vue";
-import Forget from "@/views/forget/index.vue"
-import Code from "@/views/login/code/index.vue"
+import Layout from '@/layout/index.vue'
+import type { RouteRecordRaw } from 'vue-router'
+import Home from '@/views/home/index.vue'
+import Login from '@/views/login/index.vue'
+import Forget from '@/views/forget/index.vue'
+import Code from '@/views/login/code/index.vue'
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "root",
+    path: '/first',
+    name: 'first',
+    component: () => import('@/views/first/index.vue'),
+    meta: {
+      title: '服务',
+      noCache: true
+    }
+  },
+  {
+    path: '/msg',
+    name: 'msg',
+    component: () => import('@/views/first/msg/index.vue'),
+    meta: {
+      title: '服务',
+      noCache: true
+    }
+  },
+  {
+    path: '/',
+    name: 'root',
     component: Layout,
     meta: {
-      title: "首页"
+      title: '首页'
     },
-    redirect: "Home",
+    redirect: 'Home',
     children: [
       {
-        path: "home",
-        name: "Home",
+        path: 'home',
+        name: 'Home',
         component: Home,
         meta: {
-          title: "主页"
+          title: '主页'
         }
       },
       {
-        path: "my",
-        name: "My",
-        component: () => import("@/views/my/index.vue"),
+        path: 'my',
+        name: 'My',
+        component: () => import('@/views/my/index.vue'),
         meta: {
-          title: "我的",
+          title: '我的',
           noCache: true
         }
       },
       {
-        path: "bullhorn",
-        name: "Bullhorn",
-        component: () => import("@/views/bullhorn/index.vue"),
+        path: 'bullhorn',
+        name: 'Bullhorn',
+        component: () => import('@/views/bullhorn/index.vue'),
         meta: {
-          title: "消息",
+          title: '消息',
           noCache: true
         }
       },
       {
-        path: "bullhorn",
-        name: "Open",
-        component: () => import("@/views/open/index.vue"),
+        path: 'bullhorn',
+        name: 'Open',
+        component: () => import('@/views/open/index.vue'),
         meta: {
-          title: "开门",
+          title: '开门',
           noCache: true
         }
       },
       {
-        path: "service",
-        name: "Service",
-        component: () => import("@/views/service/index.vue"),
+        path: 'service',
+        name: 'Service',
+        component: () => import('@/views/service/index.vue'),
         meta: {
-          title: "服务",
+          title: '服务',
           noCache: true
         }
-      },
+      }
     ]
   },
   {
-    path: "/login",
-    name: "login",
+    path: '/login',
+    name: 'login',
     meta: {
-      title: "登录"
+      title: '登录'
     },
     component: Login
-  }, {
-    path: "/forget",
-    name: "forget",
+  },
+  {
+    path: '/forget',
+    name: 'forget',
     meta: {
-      title: "忘记密码"
+      title: '忘记密码'
     },
     component: Forget
-  }, {
-    path: "/code",
-    name: "code",
+  },
+  {
+    path: '/code',
+    name: 'code',
     meta: {
-      title: "登录"
+      title: '登录'
     },
     component: Code
-  },
-];
+  }
+]
 
-export default routes;
+export default routes
---
+++ src/views/first/index.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="bigBox">
+		<div class="box">
+			<div class="hader">
+				<span>温馨提示</span>
+			</div>
+			<div class="content">
+				<div>
+					为了帮助您安全使用产品和服务，在您同意并授权基础上，我们可能会收集你的交易信息，位置信息，设备信息等。请你认证阅读<a
+						@click="
+							$router.push({
+								path: '/msg',
+								query: {
+									type: 0
+								}
+							})
+						"
+						>《用户服务协议》</a
+					>及<a
+						@click="
+							$router.push({
+								path: '/msg',
+								query: {
+									type: 1
+								}
+							})
+						"
+						>《隐私政策》</a
+					>的全部条款。您同意并接受全部条款后，再开始使用我们的服务。
+				</div>
+			</div>
+			<div class="bottom">
+				<button @click="no">不同意</button>
+				<button class="aggree" @click="$router.push('/login')">同意并继续</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+function no() {
+	window.location.reload()
+}
+</script>
+
+<style scoped>
+.bigBox {
+	height: 100vh;
+	width: 100vw;
+	background: #7d777b;
+	position: relative;
+}
+.box {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: #fff;
+	height: 320px;
+	width: 320px;
+	border-radius: 6px;
+	display: flex;
+	flex-direction: column;
+}
+.hader {
+	height: 60px;
+	line-height: 60px;
+	text-align: center;
+	border-bottom: 1px solid #ccc;
+	font-size: 20px;
+	font-weight: 500;
+}
+.content {
+	flex: 1;
+	border-bottom: 1px solid #ccc;
+	padding: 20px;
+	font-size: 14px;
+}
+
+.bottom {
+	height: 60px;
+	display: flex;
+	align-items: center;
+}
+.bottom button {
+	flex: 1;
+}
+.aggree {
+	color: #63b0ff;
+}
+a {
+	color: #51a7ff;
+}
+</style>
