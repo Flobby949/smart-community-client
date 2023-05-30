c4cc910c7655553fe287e8a68444e6d44d2d8a6e
aliyun2145454989 <2283582431@qq.com>
2023-05-30 11:20:21 +0800

    fix(custom): 修复路由组件问题

---
+++ src/views/home/invitation.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const invitation = ref()
+
+onMounted(() => {
+	invitation.value = route.params.invitationId
+})
+</script>
+
+<template>
+	<div>访客邀请</div>
+	<div>{{ invitation }}</div>
+</template>
