a24552a72bd03aa37bd5bc240dff2acb1c334a72
aliyun2145454989 <2283582431@qq.com>
2023-06-02 14:46:43 +0800

    feat(custom): 服务页面开发完毕 自行补充跳转页面url

---
+++ src/components/service-icon/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="list">
+		<template v-for="(item, index) in prop.list" :key="index">
+			<div class="item" @click="onClick(item)">
+				<img :src="`/src/assets/icon/${item.name}.png`" />
+				<div class="title">{{ item.title }}</div>
+			</div>
+		</template>
+	</div>
+</template>
+
+<script setup lang="ts">
+import router from '@/router'
+import type { PropType } from 'vue'
+
+const prop = defineProps({
+	list: {
+		type: Array as PropType<any[]>,
+		default: () => []
+	}
+})
+const emit = defineEmits(['myClick'])
+const onClick = (item: any) => {
+	router.push(item.url)
+}
+</script>
+
+<style scoped>
+.list {
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
+}
+.item {
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
+	width: 20%;
+	margin: 5px 6px;
+}
+.item img {
+	width: 35px;
+	height: 35px;
+}
+.title {
+	margin-top: 6px;
+	font-size: 14px;
+}
+</style>
