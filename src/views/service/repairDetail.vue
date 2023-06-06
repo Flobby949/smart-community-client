d7415173155039fd071297ec0d17a7ff116f3b98
hjh <hjh>
2023-06-06 13:25:11 +0800

    feat(custom): 报修详情，报修显示，报修添加界面全部完善

--- src/views/service/repairDetail.vue
+++ src/views/service/repairDetail.vue
@@ -8,8 +8,8 @@ import { getUserInfo } from '@/api/user'
 const route = useRoute()
 
 const repair = ref()
-const item = ref<any>('')
-const user = ref<any>('')
+const item = ref('')
+const user = ref('')
 
 onMounted(async () => {
 	repair.value = route.params.repairId
@@ -25,15 +25,15 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="h-52 bg-blue-400 rounded-bl-br-full" style="z-index: 99">
-		<van-nav-bar title="报修详情" left-arrow fixed @click-left="onClickLeft" />
-	</div>
+	<div style="background-color: #f5f5f5" class="h-[1100px]">
+		<div class="h-56 rounded-bl-br-full" style="z-index: 99; background-color: #409eff">
+			<van-nav-bar title="报修详情" left-arrow @click-left="onClickLeft" />
+		</div>
 
-	<div class="absolute top-20 w-[100%] h-[100%] bg-gray-100 m-2 rounded-[5px]">
-		<div class="borderb bg-white">
+		<div class="borderb bg-white rounded-md absolute top-[75px] w-[340px] h-[470px] ml-4 border-2 border-stone-200">
 			<div class="p-[4px]">
-				<div class="font-bold inline-block text mt-1 mb-1">{{ item.title }}</div>
-				<div class="float-right mr-3">
+				<div class="inline-block text mt-1 mb-1">{{ item.title }}</div>
+				<div class="float-right mr-3 text-center text-white mt-[6px]">
 					<div v-if="item.state == 0" class="bg-red-500 radiusButton">未处理</div>
 					<div v-if="item.state == 1" class="bg-yellow-500 radiusButton">处理中</div>
 					<div v-if="item.state == 2" class="bg-blue-500 radiusButton">已理中</div>
@@ -41,54 +41,56 @@ onMounted(async () => {
 				</div>
 			</div>
 
-			<hr />
+			<div class="border-dashed border-b border-slate-400 mt-2"></div>
+			<div class="text-[15px] ml-6 mt-4">
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">报修分类</div>
+					<div class="mr-5">
+						<div v-if="item.type == 0">公共报修</div>
+						<div v-if="item.type == 1">公共报修</div>
+					</div>
+				</div>
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">报修类型</div>
+					<div class="mr-5">
+						<div v-if="item.category == 0">路灯</div>
+						<div v-if="item.category == 1">厕所</div>
+					</div>
+				</div>
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">报修小区</div>
+					<div class="mr-5">
+						{{ item.communityName }}
+					</div>
+				</div>
 
-			<div class="flex justify-between text mt-3">
-				<div>报修分类</div>
-				<div class="mr-5">
-					<div v-if="item.type == 0">公共报修</div>
-					<div v-if="item.type == 1">公共报修</div>
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">报修时间</div>
+					<div class="mr-5">
+						{{ item.createTime }}
+					</div>
 				</div>
-			</div>
-			<div class="flex justify-between text mt-3">
-				<div>报修类型</div>
-				<div class="mr-5">
-					<div v-if="item.category == 0">路灯</div>
-					<div v-if="item.category == 1">厕所</div>
-				</div>
-			</div>
-			<div class="flex justify-between text mt-3">
-				<div>报修小区</div>
-				<div class="mr-5">
-					{{ item.communityName }}
-				</div>
-			</div>
 
-			<div class="flex justify-between text mt-3">
-				<div>报修时间</div>
-				<div class="mr-5">
-					{{ item.createTime }}
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">报修人</div>
+					<div class="mr-5">
+						{{ item.userName }}
+					</div>
 				</div>
-			</div>
 
-			<div class="flex justify-between text mt-3">
-				<div>报修人</div>
-				<div class="mr-5">
-					{{ item.userName }}
+				<div class="flex justify-between text mt-3">
+					<div style="color: #666666">手机号</div>
+					<div class="mr-5">
+						{{ user.phone }}
+					</div>
 				</div>
-			</div>
 
-			<div class="flex justify-between text mt-3">
-				<div>手机号</div>
-				<div class="mr-5">
-					{{ user.phone }}
-				</div>
+				<div class="mt-5 relative right-3">报修内容</div>
 			</div>
+			<div class="border-dashed border-b border-slate-400 mt-3"></div>
 
-			<div class="mt-3">报修内容</div>
-			<hr class="mt-2" />
 			<div>
-				<div class="text mt-2">
+				<div class="mt-3 text-[14px] ml-5 w-[300px]" style="color: #666666">
 					{{ item.content }}
 				</div>
 
@@ -100,54 +102,70 @@ onMounted(async () => {
 			</div>
 		</div>
 
-		<div>
-			<div class="mt-2 text">报修处理</div>
-			<div class="bg-white">
-				<van-steps direction="vertical" :active="3">
-					<van-step>
-						<h3>2021/05/31 10:01:45</h3>
-						<div class="text-blue-500 flex">
-							<div>报修人:</div>
-							<div class="ml-1">{{ item.userName }}</div>
-						</div>
-					</van-step>
-					<van-step>
-						<h3>2021/05/31 10:01:45</h3>
-						<div class="text-blue-500 flex">
-							<div>处理人:</div>
-							<div class="ml-1">李明轩</div>
-						</div>
-						<div class="text-blue-500 flex">
-							<div>状态:</div>
-							<div class="ml-1">待处理</div>
-						</div>
-					</van-step>
-					<van-step>
-						<h3>2021/05/31 10:01:45</h3>
-						<div class="text-blue-500 flex">
-							<div>处理人:</div>
-							<div class="ml-1">李明轩</div>
-						</div>
-						<div class="text-blue-500 flex">
-							<div>状态:</div>
-							<div class="ml-1">已完成</div>
-						</div>
+		<div class="mt-[330px] ml-4">报修处理</div>
 
-						<div class="text-blue-500 flex">
-							<div>处理内容:</div>
-							<div class="ml-1">更换电灯泡</div>
-						</div>
+		<div class="rounded-md bg-white mt-1 ml-4 w-[340px] h-[400px] flex border-2 border-stone-200">
+			<div class="mt-3 ml-5">
+				<div class="rounded-full w-8 h-8 border-[2.5px] border-blue-500 bg-white">
+					<div class="cross-icon ml-[6px] relative top-[7px]"></div>
+				</div>
+				<div class="bg-blue-500 w-[3px] h-[55px] ml-[15px]"></div>
+				<div class="rounded-full w-8 h-8 border-[2.5px] border-blue-500 bg-white">
+					<span class="checkmark ml-[10px] relative top-1"></span>
+				</div>
+				<div class="bg-blue-500 w-[3px] h-[55px] ml-[15px]"></div>
+				<div class="rounded-full w-8 h-8 border-[2.5px] border-blue-500 bg-white">
+					<span class="checkmark ml-[10px] relative top-1"></span>
+				</div>
+			</div>
+			<div class="ml-5 mt-4">
+				<div class="text-[15px]">
+					<div style="color: #666666" class="font-bold mt-1">2021/05/31 10:01:45</div>
+					<div class="text-blue-500 flex">
+						<div>报修人:</div>
+						<div class="ml-1">{{ item.userName }}</div>
+					</div>
+					<div class="text-blue-500 flex">
+						<div>状 态:</div>
+						<div class="ml-1">未处理</div>
+					</div>
+				</div>
+				<div class="mt-[17px] text-[15px]">
+					<div class="font-bold" style="color: #666666">2021/05/31 10:01:45</div>
+					<div class="text-blue-500 flex">
+						<div>处理人:</div>
+						<div class="ml-1">李明轩</div>
+					</div>
+					<div class="text-blue-500 flex">
+						<div>状态:</div>
+						<div class="ml-1">待处理</div>
+					</div>
+				</div>
+				<div style="color: #666666" class="text-[15px] mt-[20px]">
+					<div class="font-bold">2021/05/31 10:01:45</div>
+					<div class="flex mt-1">
+						<div>处理人:</div>
+						<div class="ml-1">李明轩</div>
+					</div>
+					<div class="flex mt-1">
+						<div>状态:</div>
+						<div class="ml-1">已完成</div>
+					</div>
 
-						<div class="text-blue-500 flex">
-							<div>现场照片:</div>
-							<div class="ml-1">
-								<template v-for="img in item.imgs" :key="img">
-									<img class="img" :src="img" />
-								</template>
-							</div>
+					<div class="flex mt-1">
+						<div>处理内容:</div>
+						<div class="ml-1">更换电灯泡</div>
+					</div>
+
+					<div class="flex mt-1">
+						<div>现场照片:</div>
+						<div class="ml-1 flex">
+							<template v-for="img in item.imgs" :key="img">
+								<img class="img" :src="img" />
+							</template>
 						</div>
-					</van-step>
-				</van-steps>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -162,6 +180,12 @@ onMounted(async () => {
 	display: inline-block;
 	padding: 1px 10px;
 	border-radius: 15px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 25px;
+	width: 66px;
+	font-size: 10px;
 }
 
 .bordera {
@@ -179,12 +203,45 @@ onMounted(async () => {
 	height: 30px;
 }
 
-.text {
-	text-indent: 20px;
-}
 .img {
 	width: 90px;
 	border-radius: 5px;
 	margin: 3px;
+}
+.checkmark::before {
+	content: '';
+	display: inline-block;
+	width: 7px;
+	height: 14px;
+	border: solid #0079fe;
+	border-width: 0 3px 3px 0;
+	transform: rotate(45deg);
+}
+.cross-icon {
+	width: 15px;
+	height: 15px;
+	position: relative;
+	transform: rotate(45deg);
+}
+
+.cross-icon:before,
+.cross-icon:after {
+	content: '';
+	position: absolute;
+	background-color: #0079fe;
+}
+
+.cross-icon:before {
+	width: 100%;
+	height: 2px;
+	top: 50%;
+	transform: translateY(-50%);
+}
+
+.cross-icon:after {
+	height: 100%;
+	width: 2px;
+	left: 50%;
+	transform: translateX(-50%);
 }
 </style>
